73c547c7de8dc3b84c066d9e7daa65b958251b5e
nytranna <anna.nytrova@email.cz>
2025-05-11 16:13:43 +0200

    restaurant app database

--- restaurant-app.sql
+++ restaurant-app.sql
@@ -1,4 +1,4 @@
--- Adminer 4.8.1 MySQL 8.0.42-0ubuntu0.24.04.1 dump
+-- Adminer 4.8.1 MySQL 10.11.11-MariaDB-0+deb12u1-log dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -8,23 +8,23 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 SET NAMES utf8mb4;
 
 CREATE TABLE `image` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `is_gallery` tinyint DEFAULT '0',
-  `order` int DEFAULT '0',
-  `uploaded_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `is_gallery` tinyint(4) DEFAULT 0,
+  `order` int(11) DEFAULT 0,
+  `uploaded_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
 
 
 CREATE TABLE `menu_category` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `id_menu_category` int DEFAULT NULL,
-  `menu_type` enum('daily_menu','menu','drinks') CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `order` int DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `id_menu_category` int(11) DEFAULT NULL,
+  `menu_type` enum('daily_menu','menu','drinks') DEFAULT NULL,
+  `order` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `id_upper_category` (`id_menu_category`),
   CONSTRAINT `menu_category_ibfk_1` FOREIGN KEY (`id_menu_category`) REFERENCES `menu_category` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
@@ -32,14 +32,14 @@ CREATE TABLE `menu_category` (
 
 
 CREATE TABLE `menu_item` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_menu_category` int NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `id_image` int DEFAULT NULL,
-  `order` int DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_menu_category` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `id_image` int(11) DEFAULT NULL,
+  `order` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `id_menu_category` (`id_menu_category`),
   KEY `id_image` (`id_image`),
@@ -49,12 +49,12 @@ CREATE TABLE `menu_item` (
 
 
 CREATE TABLE `menu_item_variant` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_menu_item` int NOT NULL,
-  `size` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_menu_item` int(11) NOT NULL,
+  `size` varchar(100) DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `id_menu_item` (`id_menu_item`),
   CONSTRAINT `menu_item_variant_ibfk_2` FOREIGN KEY (`id_menu_item`) REFERENCES `menu_item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
@@ -62,13 +62,13 @@ CREATE TABLE `menu_item_variant` (
 
 
 CREATE TABLE `news` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `is_shown` tinyint DEFAULT NULL,
-  `id_image` int DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `is_shown` tinyint(4) DEFAULT NULL,
+  `id_image` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `id_image` (`id_image`),
   CONSTRAINT `news_ibfk_5` FOREIGN KEY (`id_image`) REFERENCES `image` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
@@ -76,23 +76,23 @@ CREATE TABLE `news` (
 
 
 CREATE TABLE `opening_hours` (
-  `id` int NOT NULL,
-  `day` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `opening_hour` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `closing_hour` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `is_closed` tinyint DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL,
+  `day` varchar(255) NOT NULL,
+  `opening_hour` varchar(20) DEFAULT NULL,
+  `closing_hour` varchar(20) DEFAULT NULL,
+  `is_closed` tinyint(4) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
 
 
 CREATE TABLE `password_reset` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_user` int NOT NULL,
-  `hash` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `hash` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`),
   CONSTRAINT `password_reset_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
@@ -100,77 +100,77 @@ CREATE TABLE `password_reset` (
 
 
 CREATE TABLE `reservation` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `customer_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `customer_email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `customer_phone` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_name` varchar(255) NOT NULL,
+  `customer_email` varchar(255) DEFAULT NULL,
+  `customer_phone` varchar(15) NOT NULL,
   `reservation_date` datetime NOT NULL,
-  `guest_count` int NOT NULL,
-  `note` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `status` enum('pending','confirmed','cancelled') CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL DEFAULT 'pending',
-  `is_new` tinyint DEFAULT '1',
-  `email_send` tinyint DEFAULT NULL,
-  `last_user` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `guest_count` int(11) NOT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `status` enum('pending','confirmed','cancelled') NOT NULL DEFAULT 'pending',
+  `is_new` tinyint(4) DEFAULT 1,
+  `email_send` tinyint(4) DEFAULT NULL,
+  `last_user` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
 
 
 CREATE TABLE `restaurant` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `sentence` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `about_us` text CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci,
-  `ico` int DEFAULT NULL,
-  `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `phone` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `email_send` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `facebook` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `instagram` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `tripadvisor` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `menu_with_images` tinyint DEFAULT NULL,
-  `id_image` int DEFAULT NULL,
-  `id_image_events` int DEFAULT NULL,
-  `id_image_about_us` int DEFAULT NULL,
-  `id_image_about_us_in` int DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `sentence` varchar(255) DEFAULT NULL,
+  `about_us` text DEFAULT NULL,
+  `ico` int(11) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `email_send` varchar(255) DEFAULT NULL,
+  `facebook` varchar(255) DEFAULT NULL,
+  `instagram` varchar(255) DEFAULT NULL,
+  `tripadvisor` varchar(255) DEFAULT NULL,
+  `id_image` int(11) DEFAULT NULL,
+  `id_image_events` int(11) DEFAULT NULL,
+  `id_image_about_us` int(11) DEFAULT NULL,
+  `id_image_about_us_in` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `id_image` (`id_image`),
   KEY `id_image_events` (`id_image_events`),
   KEY `id_image_about_us` (`id_image_about_us`),
   KEY `id_image_about_us_in` (`id_image_about_us_in`),
   CONSTRAINT `restaurant_ibfk_2` FOREIGN KEY (`id_image`) REFERENCES `image` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `restaurant_ibfk_3` FOREIGN KEY (`id_image_events`) REFERENCES `image` (`id`),
-  CONSTRAINT `restaurant_ibfk_4` FOREIGN KEY (`id_image_about_us`) REFERENCES `image` (`id`),
-  CONSTRAINT `restaurant_ibfk_5` FOREIGN KEY (`id_image_about_us_in`) REFERENCES `image` (`id`)
+  CONSTRAINT `restaurant_ibfk_6` FOREIGN KEY (`id_image_events`) REFERENCES `image` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `restaurant_ibfk_7` FOREIGN KEY (`id_image_about_us_in`) REFERENCES `image` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `restaurant_ibfk_8` FOREIGN KEY (`id_image_about_us`) REFERENCES `image` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
 
 
 CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci DEFAULT NULL,
-  `role` enum('admin','staff') CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('admin','staff') NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
 
 
 CREATE TABLE `web_sections` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `is_shown` tinyint DEFAULT NULL,
-  `href` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_czech_ci NOT NULL,
-  `order` int NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `is_shown` tinyint(4) DEFAULT NULL,
+  `href` varchar(255) NOT NULL,
+  `order` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_czech_ci;
 
 
+-- 2025-05-11 14:12:12
